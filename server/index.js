00fef03d6a4c213dfa76b7536bba99e605f1b925
Emal Johny <emaljo573@gmail.com>
2023-06-05 19:19:33 +0530

    PMS-schemas and GraphQL

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+const { graphqlHTTP } = require('express-graphql')
+const schema=require('./schema/schema')
+const port = process.env.PORT || 5000
+const app = express()
+const connectDB =require('./config/db')
+
+connectDB()
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:process.env.NODE_ENV==='development'
+}))
+app.listen(port,()=>{
+    console.log("server running")
+})
